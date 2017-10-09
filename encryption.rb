a8c2e319528b062a320d81cb9a15ebfa320db6b9
Bryce Thuilot <bthuilot@gmail.com>
2017-10-09 14:02:59 -0400

    Formatting

--- encryption.rb
+++ encryption.rb
@@ -1,14 +1,5 @@
 #!/usr/bin/env ruby
 
-
-# Generate Random Number
-bitString = "1"
-(1...2048).each do
-  bitString += rand(2).to_s
-end
-
-# bitString.to_i(2) #=> converts to int in base 10
-
 def raiseToModulus a b n
 
 end
@@ -16,4 +7,25 @@ end
 
 def isPrime? n
   return raiseToModulus(a,n-1,n) == 1
+end
+
+
+
+# Compute based on arguments
+
+
+if ARGV[0] == "-c" or ARGV[0] == "--create-key-pair"
+  # Generate Random Number
+  bitString = "1"
+  (1...2048).each do
+    bitString += rand(2).to_s
+  end
+  # bitString.to_i(2) #=> converts to int in base 10
+
+elsif ARGV[0] == "-e" or ARGV[0] == "--encrypt"
+
+elsif ARGV[0] == "-d" or ARGV[0] == "--decrypt"
+
+else
+
 end
